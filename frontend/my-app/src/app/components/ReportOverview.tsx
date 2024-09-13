2b78228c97c0290aeada1ad2bcabb341942bafc2
Marc Tatam <marc.smsjb.tatam@gmail.com>
2024-09-13 11:14:29 +0100

    integrate backend changes

--- frontend/my-app/src/app/components/ReportOverview.tsx
+++ frontend/my-app/src/app/components/ReportOverview.tsx
@@ -4,8 +4,9 @@ import styles from '@/styles/ReportOverview.module.css'
 import { useRouter } from 'next/navigation';
 
 interface ReportProps {
-    gw_id:Number
-    headline:string
+    gw_id:Number,
+    headline:string,
+    complete:boolean
 }
 
 export default function ReportOverview(reportProps:ReportProps){
@@ -13,6 +14,16 @@ export default function ReportOverview(reportProps:ReportProps){
 
     const handleClick = () => {
         router.push('/report/'+reportProps.gw_id.toString())
+    }
+    if (!reportProps.complete){
+        return (<div className={styles.containerCurrent} onClick={handleClick}>
+            <div className={styles.weekContainer}>
+                <p className={styles.containerBody}>Week {reportProps.gw_id.toString()}</p>
+            </div>
+            <div className={styles.headingContainter}>
+                <h2 className={styles.containerHeading}>{reportProps.headline}</h2>
+            </div>
+        </div>)
     }
     return (<div className={styles.container} onClick={handleClick}>
         <div className={styles.weekContainer}>
--- frontend/my-app/src/app/components/ReportOverviewList.tsx
+++ frontend/my-app/src/app/components/ReportOverviewList.tsx
@@ -1,11 +1,55 @@
+'use client'
 import styles from '@/styles/ReportOverviewList.module.css'
 import ReportOverview from './ReportOverview'
+import { useEffect, useState } from 'react';
+
+const apiUrl = process.env.NEXT_PUBLIC_REPORT_GENERATOR_URL;
+
+async function fetchReports(){
+  const response = await fetch(`${apiUrl}/reports`)
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  const data = await response.json();
+  return data;
+}
 
 export default function ReportOverviewList(){
-    const temp = [{gw_id: 1, headline:'Hello World'},{gw_id: 2, headline:'Hello World'},{gw_id: 3, headline:'Hello World'},{gw_id: 4, headline:'Hello World'}]
+  const [reportsData, setReportsData] = useState<Array<any>>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  useEffect(() => {
+      async function getData() {
+        try{
+          const result = await fetchReports();
+          setReportsData(result);
+        } catch (error:any) {
+          console.log(error)
+          setError(error.toString())
+        } finally {
+          setIsLoading(false)
+        }
+        
+      }
+      getData();
+  }, []);
+    if (isLoading){
+      return (
+        <div className={styles.reportsContainer}>
+          Loading Reports...
+        </div>
+      );
+    }
+    if (error){
+      return (
+        <div>
+          Something went wrong while loading!
+        </div>
+      )
+    }
     return <div className={styles.reportsContainer}>
-        {temp.map((item) => (
-        <ReportOverview key={item.gw_id} gw_id={item.gw_id} headline={item.headline}/>
+        {reportsData.map((item) => (
+        <ReportOverview key={item.gw_id} gw_id={item.gw_id} headline={item.headline} complete={item.complete}/>
       ))}
     </div>
 }
