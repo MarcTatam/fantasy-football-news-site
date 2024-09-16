2fb5bfe3c680cb74f98d65c745f4189bc61e9cf8
Marc Tatam <marc.smsjb.tatam@gmail.com>
2024-09-16 21:36:24 +0100

    Fix gameweek status not updating

--- backend/football_reports/datasources/fpl.py
+++ backend/football_reports/datasources/fpl.py
@@ -6,7 +6,7 @@ class FPLClient:
     def __init__(self):
         self.league_id = 161855
 
-    def get_gameweek(self)->tuple[int,bool]:
+    def get_current_gameweek(self)->tuple[int,bool]:
         response = requests.get("https://fantasy.premierleague.com/api/bootstrap-static/")
         response.raise_for_status()
         response_dict = response.json()
@@ -14,6 +14,15 @@ class FPLClient:
             if event["is_current"]:
                 return event["id"], event["finished"]
             
+    def check_gameweek_complete(self,gw_id):
+        response = requests.get("https://fantasy.premierleague.com/api/bootstrap-static/")
+        response.raise_for_status()
+        response_dict = response.json()
+        for event in response_dict.get("events"):
+            if event["id"]==gw_id:
+                return event["finished"]
+        return False
+
     def get_teams(self)->tuple[list[dict],str]:
         response = requests.get(f"https://fantasy.premierleague.com/api/leagues-classic/{self.league_id}/standings/")
         response.raise_for_status()
--- backend/main.py
+++ backend/main.py
@@ -35,7 +35,7 @@ def pre_flight():
 @app.get("/")
 def create_report_endpoint(response:Response)->Report:
     response.headers.update({"Access-Control-Allow-Origin": "*"})
-    gw_id, is_complete = fpl_client.get_gameweek()
+    gw_id, is_complete = fpl_client.get_current_gameweek()
     if is_complete:
         db_result = firestore_client.get_report_from_db(gw_id)
         if db_result:
@@ -48,7 +48,7 @@ def create_report_endpoint(response:Response)->Report:
 async def get_all_reports_endpoint(response: Response) -> list[ReportResponse]:
     response.headers.update({"Access-Control-Allow-Origin": "*"})
     reports = firestore_client.get_all_reports_from_db()
-    gw_id, _ = fpl_client.get_gameweek()
+    gw_id, _ = fpl_client.get_current_gameweek()
     
     if len(reports) != gw_id:
         missing = set(range(1, gw_id + 1))
@@ -82,7 +82,8 @@ def get_report_endpoint(gw_id:int, response:Response)->Report:
     if db_result:
         return db_result
     generated_report = create_report(gw_id, fpl_client, formatter,report_generator)
-    firestore_client.add_report_to_db(generated_report, gw_id, False)
+    complete = fpl_client.check_gameweek_complete(gw_id)
+    firestore_client.add_report_to_db(generated_report, gw_id, complete)
     return generated_report
 
 @app.get('/summary')
